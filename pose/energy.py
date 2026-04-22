75c770407da0dff6b5119bfd8d7b74f2ca50ccd9
sarisabban <sari.sabban@gmail.com>
2026-04-22 13:14:52 +0300

    Added basic energy functions

--- pose/energy.py
+++ pose/energy.py
@@ -1,15 +1,157 @@
 import numpy as np
 
-def Lennard_Jones(pose, terms):
+def harmonic_bond(r, r0, Kb):
 	'''
-	Total Lennard-Jones 12-6 potential energy (kcal/mol)
+	Calculates the Harmonic Bond Stretching potential energy between two atoms
 	Arguments:
 	----------
-		pose  : Pose
-		terms : (sigma [Angstrom], epsilon [kcal/mol], mask [bool])
+		r:  Current bond length between two atoms
+		r0: The bond length between the two atoms at equilibrium
+		Kb: The force constant
 	Returns:
 	--------
-		float : total LJ energy in kcal/mol
+		float: potential energy in kcal/mol
+	'''
+	return Kb * (r - r0)**2
+
+def morse_potential(r, r0, De, a):
+	'''
+	Calculates Morse Bond potential energy between two atoms
+	Arguments:
+	----------
+		r:  Current bond length between two atoms
+		r0: The bond length between the two atoms at equilibrium
+		De: Well depth (strength)
+		a:  Well width (smoothness)
+	Returns:
+	--------
+		float: potential energy in kcal/mol
+	'''
+	return De * (1 - np.exp(-a * (r - r0)))**2
+
+def harmonic_angle(theta, theta0, K_theta):
+	'''
+	Calculates Harmonic Angle Bending potential energy between two atoms
+	Arguments:
+	----------
+		theta:   Current bond angle between two atoms (rad)
+		theta0:  The bond angle between two atoms at equilibrium (rad)
+		K_theta: Force constant
+	Returns:
+	--------
+		float: potential energy in kcal/mol
+	'''
+	return K_theta * (theta - theta0)**2
+
+
+
+def dihedral_potential(phi, phi0, k_phi, n):
+    """
+    Calculates Torsional (Dihedral) Potential energy.
+    phi:   current dihedral angle (radians)
+    phi0:  phase offset/target angle (radians)
+    k_phi: force constant (barrier height)
+    n:     multiplicity (number of peaks in 360 degrees)
+    """
+    return k_phi * (1 + np.cos(n * phi - phi0))
+
+
+def improper_dihedral_potential(psi, psi0, k_imp):
+    """
+    Calculates Improper Dihedral energy.
+    psi: current out-of-plane angle, psi0: target angle, k_imp: force constant
+    """
+    return k_imp * (psi - psi0)**2
+
+
+def electrostatic_potential(r, q1, q2, epsilon_r=1.0):
+    """
+    Calculates Coulombic Potential energy.
+    r: distance between atoms
+    q1, q2: partial charges of the atoms
+    epsilon_r: dielectric constant (default 1.0 for vacuum/explicit solvent)
+    """
+    # Coulomb constant (k_e) in kcal*A/(mol*e^2) is approx 332.06
+    ke = 332.06 
+    return (ke * q1 * q2) / (epsilon_r * r)
+
+
+
+
+def lennard_jones(r, sigma, epsilon, variant='12-6'):
+	'''
+	Calculates Lennard-Jones potential energy between two atoms
+	Arguments:
+	----------
+		r:       Current bond length between two atoms
+		sigma:   Distance where energy is zero (or energy minima in 9-6 variant)
+		epsilon: Well depth (strength)
+	Returns:
+	--------
+		float: potential energy in kcal/mol
+	'''
+	if variant == '12-6':
+		ratio_12 = (sigma / r)**12
+		ratio_6  = (sigma / r)**6
+		return 4 * epsilon * (ratio_12 - ratio_6)
+	elif variant == '9-6':
+		ratio_9 = 2 * (sigma / r)**9
+		ratio_6 = 3 * (sigma / r)**6
+		return epsilon * (ratio_9 - ratio_6)
+	else:
+		raise Exception('Incorrect variant type')
+
+
+
+import numpy as np
+
+def scaled_14_interaction(r, sigma, epsilon, q1, q2, f_lj=0.5, f_elec=0.833):
+    """
+    Applies scaling factors to non-bonded interactions between 1-4 atom pairs.
+    f_lj: scaling for Lennard-Jones (default 0.5 for AMBER)
+    f_elec: scaling for Electrostatics (default 0.833 for AMBER)
+    """
+    # Reuse your existing LJ and Coulomb functions
+    e_lj = lennard_jones(r, sigma, epsilon)
+    e_elec = coulomb_potential(r, q1, q2)
+    
+    return (e_lj * f_lj) + (e_elec * f_elec)
+
+def urey_bradley(r13, s0, k_ub):
+    """
+    Calculates Urey-Bradley energy (Harmonic 1-3 interaction).
+    r13: distance between the 1st and 3rd atoms in an angle
+    s0: equilibrium distance, k_ub: force constant
+    """
+    return k_ub * (r13 - s0)**2
+
+def drude_self_energy(d, k_drude):
+    """
+    Calculates the harmonic 'self-energy' of a Drude oscillator.
+    d: distance between the Drude particle and its parent atom core
+    k_drude: the spring constant (polarizability constant)
+    """
+    return 0.5 * k_drude * d**2
+
+
+
+
+
+
+
+
+
+
+def Lennard_Jones(pose, terms):
+	'''
+	Total Lennard-Jones 12-6 potential energy
+	Arguments:
+	----------
+		pose:  Pose()
+		terms: (sigma [Angstrom], epsilon [kcal/mol], mask [bool])
+	Returns:
+	--------
+		float: potential energy in kcal/mol
 	'''
 	sigma, epsilon, mask = terms
 	coords = np.asarray(pose.data['Coordinates'], dtype=np.float64)
